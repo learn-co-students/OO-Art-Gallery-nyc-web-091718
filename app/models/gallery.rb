73fb025767310a204404efdbff100a2839bc9d26
srscalia <shelby.scalia@gmail.com>
2018-09-25 11:05:19 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,10 +2,46 @@ class Gallery
 
   attr_reader :name, :city
 
-def initialize(name, city)
-  @name = name
-  @city = city
-end
+  @@all = []
 
+  def self.all
+    @@all
+  end
+
+  def initialize(name, city)
+    @name = name
+    @city = city
+
+    @@all << self
+  end
+
+  def self.cities
+    self.all.map do |gallery|
+      gallery.city.uniq
+    end
+  end
+
+  def artists
+    gp = Painting.all.select do |painting|
+      painting.gallery == self
+    end
+    gp.map do |painting|
+      painting.artist
+    end
+  end
+
+  def names
+    artists.map do |artist|
+      artist.name
+    end
+  end
+
+  def combined_years
+    total = 0
+    artists.each do |artist|
+      total += artist.years_active
+    end
+    total
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
+g1 = Gallery.new("CoCo's", "Paris")
+g2 = Gallery.new("Sun Gallary", "DC")
+g3 = Gallery.new("CoCo's", "NY")
+
+a1 = Artist.new("Bob", 45)
+a2 = Artist.new("Bill", 5)
+a3 = Artist.new("Bee", 12)
+
+p1 = Painting.new("title", "Impressionist", a1, g1)
+p2 = Painting.new("title", "Modern", a2, g2)
+p3 = Painting.new("title", "Impressionist", a1, g2)
+p4 = Painting.new("title", "Impressionist", a3, g3)
+
+
+
+
+
 binding.pry
